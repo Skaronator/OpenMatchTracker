0d6a1055f572c46e591334a2ac9626a323532c34
Niklas Wagner <Skaro@Skaronator.com>
2016-07-26 19:09:29 +0200

    Add more Default data

--- SQL/db-data.sql
+++ SQL/db-data.sql
@@ -2,3 +2,8 @@
 INSERT INTO `omt_side` (`sid`, `shortname`, `name`) VALUES
 (1, 'T', 'Terrorist'),
 (2, 'CT', 'Counter-Terrorist');
+
+
+INSERT INTO `omt_role`(`rid`, `name`) VALUES
+(1, 'Main Roster'),
+(2, 'Stand-in');
