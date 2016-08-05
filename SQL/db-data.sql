e87ccee5213aacffe2866235d08a6e50075d6d21
Niklas Wagner <Skaro@Skaronator.com>
2016-08-05 02:19:51 +0200

    update default data

--- SQL/db-data.sql
+++ SQL/db-data.sql
@@ -1,7 +1,7 @@
-
-INSERT INTO `omt_side` (`sid`, `shortname`, `name`) VALUES
-(1, 'T', 'Terrorist'),
-(2, 'CT', 'Counter-Terrorist');
+INSERT INTO `omt_host` (`hid`, `name`, `img`) VALUES
+(1, 'Valve', NULL),
+(2, 'ESEA', NULL),
+(2, 'FACEIT', NULL);
 
 
 INSERT INTO `omt_role`(`rid`, `name`) VALUES
